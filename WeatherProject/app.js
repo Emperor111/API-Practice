b63cf75bc0361827ef97bbd8da24c2ccdb23c069
Eshan <eshantiwari111@gmail.com>
2023-07-01 20:04:38 +0530

    Update

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -16,8 +16,8 @@ app.get("/", function(req, res){
             const temp = weatherData.main.temp
             //console.log(temp);
             const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[1].icon
-            const imageURL = "https://openweathermap.org/img/wn" + icon + "@2x.png"
+            const icon = weatherData.weather[0].icon
+            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<p>The weather is currently " + weatherDescription + "</p>")
             res.write("<h1>The temparture in gurugram is " + temp + "degree Celcius.</h1>")
             res.write("<img src=" + imageURL + ">")
