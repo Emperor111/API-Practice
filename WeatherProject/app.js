69666295edfeb59aff70b010ab8b871e44d42633
Eshan <eshantiwari111@gmail.com>
2023-07-02 22:49:49 +0530

    Completed

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,12 +1,21 @@
 //jshint esversion:6
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({encoded: true}));
+
 app.get("/", function(req, res){
-    
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Gurugram&appid=e812f1522c15640288b552b716139b71&units=metric"
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "";
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit
     https.get(url, function(response) {
         console.log(response.statusCode);
         
@@ -14,18 +23,17 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data);
             //console.log(weatherData);
             const temp = weatherData.main.temp
-            //console.log(temp);
+            console.log(temp);
             const weatherDescription = weatherData.weather[0].description
             const icon = weatherData.weather[0].icon
             const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.write("<p>The weather is currently " + weatherDescription + "</p>")
-            res.write("<h1>The temparture in gurugram is " + temp + "degree Celcius.</h1>")
+            res.write("<h1>The temparture in " + query + " is " + temp + " degree Celcius.</h1>")
             res.write("<img src=" + imageURL + ">")
             res.send()
         })
     });
 })
-
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
